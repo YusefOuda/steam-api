973a5a6cce455fb7dfa455b4e5acb5098a8c2a34
Yusef Ouda <yiouda92@gmail.com>
2014-10-29 13:38:51 -0500

    add tests for urls module (friend_list and player_bans)

---
+++ spec/steam_api/urls_spec.rb
@@ -0,0 +1,13 @@
+describe SteamApi::Urls do
+  describe '.friend_list' do
+    it 'returns the base url for getting users friends list (with passed in id)' do
+      result = SteamApi::Urls.friend_list('938457325', 'all')
+      expect(result).to be_a(String)
+    end
+
+    it 'returns the base url for getting info about player bans (passed in id)' do
+      result = SteamApi::Urls.player_bans('938457325')
+      expect(result).to be_a(String)
+    end
+  end
+end
